3d1bb0607f051a96eec1849a6e41b6c4d35e1b87
Funmi B <osb1271.rit@gmail.com>
2024-01-23 12:50:24 -0500

    add exception handling

--- import-skills-boost.py
+++ import-skills-boost.py
@@ -2,29 +2,32 @@ from bs4 import BeautifulSoup
 import re
 from urllib import request
 
-with request.urlopen('https://bit.ly/gcp-bab501a') as f:
-    contents = f.read()
+try:
+    with request.urlopen('https://bit.ly/gcp-bab501a') as f:
+        contents = f.read()
 
-    soup = BeautifulSoup(contents, 'html.parser')
+        soup = BeautifulSoup(contents, 'html.parser')
 
-    badges = soup.find('div', attrs={'class': 'profile-badges'})
+        badges = soup.find('div', attrs={'class': 'profile-badges'})
 
-    badge_data = dict()
-    
-    for badgeEl in badges:
-        badge = badgeEl.findNext('span')
-        badgeName = badge.text.strip('\r\n')
-        badgeName = re.sub('\s\s+' , ' ', badgeName)
+        badge_data = dict()
+        
+        for badgeEl in badges:
+            badge = badgeEl.findNext('span')
+            badgeName = badge.text.strip('\r\n')
+            badgeName = re.sub('\s\s+' , ' ', badgeName)
 
-        if badgeName != '':
-            completion = badge.find_next_sibling().text.strip('\r\n')
-            completion = re.sub('\s\s+' , ' ', completion)
+            if badgeName != '':
+                completion = badge.find_next_sibling().text.strip('\r\n')
+                completion = re.sub('\s\s+' , ' ', completion)
 
-            # Add styling to badge thumbnail
-            thumbnail = badgeEl.findNext('a')
-            thumbnail.find('img').attrs['style'] = 'max-width: 10rem;'
+                # Add styling to badge thumbnail
+                thumbnail = badgeEl.findNext('a')
+                thumbnail.find('img').attrs['style'] = 'max-width: 10rem;'
 
-            badge_data[badgeName] = [thumbnail, completion]
+                badge_data[badgeName] = [thumbnail, completion]
 
-    print(len(badge_data))
-    print(badge_data)
+        print('{} badge(s) found.'.format(len(badge_data)))
+
+except Exception as e:
+    print("An error occurred: ", e)
