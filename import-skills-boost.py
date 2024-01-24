14aeee1f1d9bcc86b44d4f256cfc736e44e60555
Funmi B <osb1271.rit@gmail.com>
2024-01-23 20:39:25 -0500

    add limit arg

--- import-skills-boost.py
+++ import-skills-boost.py
@@ -1,3 +1,4 @@
+from sys import argv
 from bs4 import BeautifulSoup
 import re
 from urllib import request
@@ -28,6 +29,16 @@ try:
                 badge_data[badgeName] = [thumbnail, completion]
 
         print('{} badge(s) found.'.format(len(badge_data)))
+
+        if len(argv) == 1:
+            limit = 3
+            print('Limit has been set to default of {}.'.format(limit))
+        else:
+            limit = int(argv[1])
+
+        print('Up to {} badge(s) will be printed.\n'.format(limit))
+
+        generate_readme_text(badge_data, limit)
 
 except Exception as e:
     print("An error occurred: ", e)
